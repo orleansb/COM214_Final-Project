9ec6d00b38d4d43ebc528637fb2443b277cdc721
August104 <august104884@gmail.com>
2024-05-02 12:57:48 -0400

    Update Homepage animaiton

--- Business Case/Homepage/homepage.js
+++ Business Case/Homepage/homepage.js
@@ -1,9 +1,11 @@
 window.onload = () => {
     const jumper = document.getElementById('jumper');
     const container = document.querySelector('.letter-container');
+    const textcontainer = document.querySelector('.text-container')
 
     jumper.addEventListener('animationend', () => {
         console.log('Jumper has finished the jump.');
         container.style.animation = 'moveViewDown 1s forwards';
+        textcontainer.style.animation = 'moveUp 1s forwards';
     });
 };
